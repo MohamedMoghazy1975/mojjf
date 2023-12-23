222dc06e96d1e0e42b80062acec784c3773a8c7a
MohamedMoghazy1975 <154369703+MohamedMoghazy1975@users.noreply.github.com>
2023-12-23 22:08:00 +0000

    adding dynamic data and create api
    adding dynamic data and create api for me

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-
+JOBS = [
+  {
+  'id': 1,
+  'name': 'amin',
+  'password': '123'
+  },
+{
+'id': 2,
+'name': 'koko',
+'password': '123'
+  }
+]
 @app.route("/")
 def hello_world():
-  return  render_template ('home.html')
+  return  render_template ('home.html', 
+                           jobs=JOBS,
+                          mycountry='وزارة العدل')
 
 
-
+@app.route("/api/mydataapi")
+def datalist():
+  return jsonify(JOBS )
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000, debug=True)
